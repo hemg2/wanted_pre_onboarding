c739ba8be44285669762a4e2b625580efb052ac7
hemg2 <hemg2@naver.com>
2022-09-13 20:56:46 +0900

    깃 추가

---
+++ wanted-pre-onboarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wanted-pre-onboarding
+//
+//  Created by 1 on 2022/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
